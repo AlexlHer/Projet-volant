cc660258e25462b9acabbaad2cdfeb8dabb012a9
Alexandre <alexandre.alex10@hotmail.fr>
2017-03-01 16:35:01 +0100

    Program de reconfig moteur en cas de plantage v1.0

--- Reconfiguration.py
+++ Reconfiguration.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#Auteur : Alexandreou
+#Auteur : Alexandre l'Heritier
 print("----------------------------------------------------------------------")
 print("Reconfiguration v1.0")
 print("----------------------------------------------------------------------")
