16fd1e235a3c6b811a5ae7b45f999adca633c237
Alexandre <alexandre.alex10@hotmail.fr>
2017-03-01 16:35:32 +0100

    Programme test

--- Configurationinitial.py
+++ Configurationinitial.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-#Auteur : Alexandreou
+#Auteur : Alexandre l'Heritier
 print("----------------------------------------------------------------------")
 print("Configuration initial")
 print("----------------------------------------------------------------------")
